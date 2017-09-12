647ee114997c84468a375ecf3c3a7798215ed218
Brandon Little <brlittle86@gmail.com>
2017-09-12 16:30:27 -0700

    completed initial code challenge; used System.Linq to allow reversal of string as array

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string to be reversed:");
+            StringReversal(Console.ReadLine());
+        }
+
+        static void StringReversal(string entry)
+        {
+            Console.WriteLine(new string(entry.ToCharArray().Reverse().ToArray()));
+            Console.Read();
+        }
+
+
+    }
+}
