0d810a6ac265db38a82824ce201af5db12697d34
Brandon Little <brlittle86@gmail.com>
2017-09-14 08:25:27 -0700

    added the carrot and tomato garden

--- CatGarden/CatGarden/Program.cs
+++ CatGarden/CatGarden/Program.cs
@@ -23,7 +23,24 @@ namespace CatGarden
                 }
                     Console.WriteLine();
             }
+            
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.Write("Carrot ");
+                    }
+                    else
+                    {
+                        Console.Write("Tomato ");
+                    }
+                }
+                Console.WriteLine();
+            }
             Console.Read();
         }
+
     }
 }
