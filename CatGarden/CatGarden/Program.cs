cdad8d25d4d28fbd6c100d7b742ce7d66313ee5c
Brandon Little <brlittle86@gmail.com>
2017-09-13 17:05:30 -0700

    populates an array of arrays with arrays of strings in an alternating pattern and then writes to the console in order

---
+++ CatGarden/CatGarden/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CatGarden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making a Cat Garden");
+
+            Array[] catGarden = CatGarden();
+            foreach (var item in catGarden)
+            {
+                foreach (var thing in item)
+                {
+                    Console.WriteLine(thing);
+                }
+            }
+            Console.Read();
+        }
+
+        static Array[] CatGarden()
+        {
+            Array[] garden = new Array[5];
+            for (int i = 0; i < garden.Length; i++)
+            {
+                if ((i + 1) % 2 != 0)
+                {
+                    string[] rowCatnip = new string[] { "catnip", "catnip", "catnip", "catnip", "catnip" };
+                    garden[i] = rowCatnip;
+                }
+                else
+                {
+                    string[] rowMint = new string[] { "mint", "mint", "mint", "mint", "mint" };
+                    garden[i] = rowMint;
+                }
+            }
+
+            return garden;
+        }
+    }
+}
