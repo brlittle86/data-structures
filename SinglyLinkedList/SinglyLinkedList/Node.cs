6808af961975a4c1b4b4ab9e16c544c760b113e2
Brandon Little <brlittle86@gmail.com>
2017-09-18 18:11:15 -0700

    implemented basic LinkedList, with AddNode, ViewLinkedList, and AddToFirst methods.

---
+++ SinglyLinkedList/SinglyLinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SinglyLinkedList
+{
+    class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
