056c352743f7ef74a29d5878995bae86d72b8231
Brandon Little <brlittle86@gmail.com>
2017-09-20 17:06:05 -0700

    starting point for code challenge

---
+++ NthNodeFromEnd/NthNodeFromEnd/LinkedList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NthNodeFromEnd
+{
+    class LinkedList
+    {
+        public Node Next { get; set; }
+
+        public LinkedList(int data)
+        {
+            Next = new Node(data);
+        }
+
+        public void AddNode(int data)
+        {
+            Node runner = Next;
+            while (runner.Next != null)
+            {
+                runner = runner.Next;
+            }
+            runner.Next = new Node(data);
+        }
+
+        public void ViewLinkedList()
+        {
+            Node runner = Next;
+            string printOut = "";
+            while (runner.Next != null)
+            {
+                printOut += runner.Data + " -> ";
+                runner = runner.Next;
+            }
+            printOut += runner.Data + " -> null";
+            Console.WriteLine(printOut);
+        }
+
+        public void AddToFirst(int data)
+        {
+            Node runner = Next;
+            Next = new Node(data);
+            Next.Next = runner;
+        }
+
+        public void FindMiddleNode()
+        {
+            Node runner = Next;
+            Node walker = Next;
+            int counter = 1;
+            while (runner.Next != null)
+            {
+                walker = walker.Next;
+                runner = runner.Next;
+                if (runner.Next != null)
+                {
+                    runner = runner.Next;
+                }
+                counter++;
+            }
+            Console.WriteLine($"The middle of this Linked List is the #{counter} node, and contains the data: {walker.Data}.");
+        }
+    }
+}
