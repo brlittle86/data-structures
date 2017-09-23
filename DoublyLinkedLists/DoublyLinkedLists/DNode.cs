31a17816e337ab6706b050bc3f534a372d68dda0
Brandon Little <brlittle86@gmail.com>
2017-09-23 00:09:03 -0700

    initial solution creation; implemented a DNode for doubly linked list, with constructor

---
+++ DoublyLinkedLists/DoublyLinkedLists/DNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedLists
+{
+    class DNode
+    {
+        public int Data { get; set; }
+        public DNode Next { get; set; }
+        public DNode Previous { get; set; }
+
+        public DNode(int data)
+        {
+            Data = data;
+        }
+    }
+}
