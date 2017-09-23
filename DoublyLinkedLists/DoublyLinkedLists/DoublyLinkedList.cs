be32c96099c83402e36087b28bc74d39ef6d91b6
Brandon Little <brlittle86@gmail.com>
2017-09-23 00:10:28 -0700

    created DoublyLinkedList class, implemented Head property and constructor

---
+++ DoublyLinkedLists/DoublyLinkedLists/DoublyLinkedList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedLists
+{
+    class DoublyLinkedList
+    {
+        public DNode Head { get; set; }
+
+        public DoublyLinkedList(int data)
+        {
+            Head = new DNode(data);
+        }
+    }
+}
