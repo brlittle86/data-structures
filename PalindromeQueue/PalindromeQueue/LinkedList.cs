24bd38448d00327c2619fe41720a2df610e77cd0
Brandon Little <brlittle86@gmail.com>
2017-09-27 17:36:58 -0700

    implemented Queue and LinkedList classes

--- PalindromeQueue/PalindromeQueue/LinkedList.cs
+++ PalindromeQueue/PalindromeQueue/LinkedList.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace PalindromeQueue
 {
-    class LinkedList
+    abstract class LinkedList
     {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public void View()
+        {
+            Node<T> runner = Head;
+            string output = "";
+            while (runner.Next != null)
+            {
+                output += runner.Data + " -> ";
+                runner = runner.Next;
+            }
+            output += runner.Data + " -> null";
+            Console.WriteLine(output);
+        }
     }
 }
--- PalindromeQueue/PalindromeQueue/Queue.cs
+++ PalindromeQueue/PalindromeQueue/Queue.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace PalindromeQueue
 {
-    class Queue
+    class Queue : LinkedList
     {
+        public Queue(string data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+
+        public void Enqueue(string data)
+        {
+            Tail.Next = new Node(data);
+            Tail = Tail.Next;
+        }
+
+        public string Dequeue()
+        {
+            if (Head == Tail) { Tail = null; }
+            Node temp = Head.Next;
+            Head.Next = null;
+            string value = Head.Data;
+            Head = temp;
+            return value;
+        }
     }
 }
