d729843db837eaa0159169e9724072381a723a99
Brandon Little <brlittle86@gmail.com>
2017-09-28 17:07:19 -0700

    initial solution creation

---
+++ TwoStacksOneQueue/TwoStacksOneQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoStacksOneQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
