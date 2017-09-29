010cb9c67f9b113d49e1e866a9f400eca92bea15
Brandon Little <brlittle86@gmail.com>
2017-09-28 17:09:44 -0700

    created Stack, Queue, and Node class files; implemented Node

---
+++ TwoStacksOneQueue/TwoStacksOneQueue/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoStacksOneQueue
+{
+    class Node
+    {
+        public string Data { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+
+        public Node(string data)
+        {
+            Data = data;
+            Next = null;
+            Previous = null;
+        }
+    }
+}
