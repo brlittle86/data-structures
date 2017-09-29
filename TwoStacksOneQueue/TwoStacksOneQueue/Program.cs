59769456d9ade04c0e04072003336e0f6634aea2
Brandon Little <brlittle86@gmail.com>
2017-09-28 17:37:03 -0700

    Filled out the Program to illustrate my implementations

--- TwoStacksOneQueue/TwoStacksOneQueue/Program.cs
+++ TwoStacksOneQueue/TwoStacksOneQueue/Program.cs
@@ -6,7 +6,20 @@ namespace TwoStacksOneQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue awesomeQueue = new Queue(1);
+            awesomeQueue.Enqueue(2);
+            awesomeQueue.Enqueue(3);
+            awesomeQueue.Stack1.View();
+            Console.WriteLine("Let's add a couple more things to the queue.\n");
+            awesomeQueue.Enqueue(4);
+            awesomeQueue.Enqueue(5);
+            awesomeQueue.Stack1.View();
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{ awesomeQueue.Dequeue().Data} was dequeued!");
+            awesomeQueue.Stack1.View();
+            Console.Read();
         }
     }
 }
