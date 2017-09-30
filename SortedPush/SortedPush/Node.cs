37d543d35689203b0e2444cff71c850a7ac06ffe
Brandon Little <brlittle86@gmail.com>
2017-09-29 17:12:15 -0700

    implemented Node

--- SortedPush/SortedPush/Node.cs
+++ SortedPush/SortedPush/Node.cs
@@ -6,5 +6,15 @@ namespace SortedPush
 {
     class Node
     {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+            Previous = null;
+        }
     }
 }
