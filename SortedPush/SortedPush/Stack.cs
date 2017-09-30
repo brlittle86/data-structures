d0fe98a8334066fc9d3fbbc7ab808bc95e97a106
Brandon Little <brlittle86@gmail.com>
2017-09-29 17:19:23 -0700

    implemented LinkedList abstract and Stack class; gave first draft on SortedPush()

--- SortedPush/SortedPush/Stack.cs
+++ SortedPush/SortedPush/Stack.cs
@@ -4,7 +4,51 @@ using System.Text;
 
 namespace SortedPush
 {
-    class Stack
+    class Stack : LinkedList
     {
+        public Stack(int data)
+        {
+            Head = new Node(data);
+            Tail = Head;
+        }
+
+        public Stack()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public void Push(int data)
+        {
+            Node temp = Head;
+            Head = new Node(data) { Next = temp };
+        }
+
+        public Node Pop()
+        {
+            if (Head == Tail) { Tail = null; }
+            Node temp = Head.Next;
+            Head.Next = null;
+            Node value = Head;
+            Head = temp;
+            return value;
+        }
+
+        public void SortedPush(int data)
+        {
+            List<int> tempList = new List<int>
+            {
+                data
+            };
+            while (Head != null)
+            {
+                tempList.Add(Pop().Data);
+            }
+            tempList.Sort();
+            foreach (int d in tempList)
+            {
+                Push(d);
+            }
+        }
     }
 }
