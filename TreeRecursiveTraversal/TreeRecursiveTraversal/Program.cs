d54316b106b3f87799b4324558718d3bd87b413e
Brandon Little <brlittle86@gmail.com>
2017-10-03 16:41:32 -0700

    Modification to Tree instantiation for clarity and awesomeness. Added comment to explain data.

--- TreeRecursiveTraversal/TreeRecursiveTraversal/Program.cs
+++ TreeRecursiveTraversal/TreeRecursiveTraversal/Program.cs
@@ -6,7 +6,22 @@ namespace TreeRecursiveTraversal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tree myTree = new Tree(3)
+            {
+                Left = new Tree(5)
+                {
+                    Left = new Tree(8),
+                    Right = new Tree(15)
+                },
+                Right = new Tree(4)
+                {
+                    Left = new Tree(31),
+                    Right = new Tree(1)
+                }
+            };
+            //Root is 3, with children 5 and 4. 5 has children 8 and 15, 4 has children 31 and 1... Order of display should be 8, 15, 5, 31, 1, 4, 3
+            myTree.Traversal();
+            Console.Read();
         }
     }
 }
