ae07cd5b64de2a838030b4b4a5836a6556e7d9a1
Brandon Little <brlittle86@gmail.com>
2017-10-03 16:32:03 -0700

    initial file creation

---
+++ TreeRecursiveTraversal/TreeRecursiveTraversal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreeRecursiveTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
