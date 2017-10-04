b546067630260bc43a675ac111807f5003a85f62
Brandon Little <brlittle86@gmail.com>
2017-10-04 16:50:13 -0700

    created program actions to run to demonstrate the different traversals

--- InOrderPostOrder/InOrderPostOrder/Program.cs
+++ InOrderPostOrder/InOrderPostOrder/Program.cs
@@ -6,7 +6,32 @@ namespace InOrderPostOrder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tree myTree = new Tree(1)
+            {
+                Left = new Tree(2)
+                {
+                    Left = new Tree(3),
+                    Right = new Tree(4)
+                },
+                Right = new Tree(5)
+                {
+                    Left = new Tree(6),
+                    Right = new Tree(7)
+                }
+            };
+
+            //Pre Order Traversal
+            Console.WriteLine("Pre-Order Traversal:");
+            myTree.PreOrderTraversal();
+
+            //In Order Traversal
+            Console.WriteLine("In-Order Traversal:");
+            myTree.InOrderTraversal();
+
+            //Post Order Traversal
+            Console.WriteLine("Post-Order Traversal:");
+            myTree.PostOrderTraversal();
+            Console.Read();
         }
     }
 }
