cb3993d4b8c6cad2dcc7cd50f2b25338f0a82b91
Brandon Little <brlittle86@gmail.com>
2017-10-05 17:25:55 -0700

    wrote up a basic Tree for demonstration

--- BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
+++ BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
@@ -6,7 +6,19 @@ namespace BreadthFirstTraversal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tree myTree = new Tree(1)
+            {
+                Left = new Tree(2)
+                {
+                    Left = new Tree(3),
+                    Right = new Tree(4)
+                },
+                Right = new Tree(5)
+                {
+                    Left = new Tree(6),
+                    Right = new Tree(7)
+                }
+            };
         }
     }
 }
