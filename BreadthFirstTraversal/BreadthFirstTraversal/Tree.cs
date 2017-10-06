6354d597302ded3815c51e53ad8826826fb15e3e
Brandon Little <brlittle86@gmail.com>
2017-10-05 17:21:08 -0700

    implemented basic Tree class

---
+++ BreadthFirstTraversal/BreadthFirstTraversal/Tree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirstTraversal
+{
+    class Tree
+    {
+        public int Data { get; set; }
+        public Tree Left { get; set; }
+        public Tree Right { get; set; }
+
+        public Tree(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
