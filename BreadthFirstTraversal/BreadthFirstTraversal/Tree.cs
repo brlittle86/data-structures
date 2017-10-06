4f81ef008925fa13ccfae3b0b481974f1af92a9f
Brandon Little <brlittle86@gmail.com>
2017-10-05 17:25:11 -0700

    implemented BreadthTraversal method on Tree class

--- BreadthFirstTraversal/BreadthFirstTraversal/Tree.cs
+++ BreadthFirstTraversal/BreadthFirstTraversal/Tree.cs
@@ -16,5 +16,25 @@ namespace BreadthFirstTraversal
             Left = null;
             Right = null;
         }
+
+        public void BreadthTraversal()
+        {
+            Queue<Tree> queue = new Queue<Tree>();
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                Tree temp = queue.Dequeue();
+                Console.Write(temp.Data + " ");
+
+                if (temp.Left != null)
+                {
+                    queue.Enqueue(temp.Left);
+                }
+                if (temp.Right != null)
+                {
+                    queue.Enqueue(temp.Right);
+                }
+            }
+        }
     }
 }
