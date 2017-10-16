73c915e86c978173399826ccff54ad2429743b89
Brandon Little <brlittle86@gmail.com>
2017-10-16 16:28:53 -0700

    basic files created; implemented basic tree class

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public int Data { get; set; }
+        public BinarySearchTree Left { get; set; }
+        public BinarySearchTree Right { get; set; }
+
+        public BinarySearchTree(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
