058e3e3172fa2bb56ec8a1d21ee18bd0bd20b901
Brandon Little <brlittle86@gmail.com>
2017-10-16 16:31:31 -0700

    added traversals for our BST

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -16,5 +16,46 @@ namespace BinarySearchTree
             Left = null;
             Right = null;
         }
+
+        public void PreOrderTraversal()
+        {
+            Console.WriteLine(Data);
+            if (Left != null) { Left.PreOrderTraversal(); }
+            if (Right != null) { Right.PreOrderTraversal(); }
+        }
+
+        public void InOrderTraversal()
+        {
+            if (Left != null) { Left.PreOrderTraversal(); }
+            Console.WriteLine(Data);
+            if (Right != null) { Right.PreOrderTraversal(); }
+        }
+
+        public void PostOrderTraversal()
+        {
+            if (Left != null) { Left.PreOrderTraversal(); }
+            if (Right != null) { Right.PreOrderTraversal(); }
+            Console.WriteLine(Data);
+        }
+
+        public void BreadthTraversal()
+        {
+            Queue<BinarySearchTree> queue = new Queue<BinarySearchTree>();
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                BinarySearchTree temp = queue.Dequeue();
+                Console.Write(temp.Data + " ");
+
+                if (temp.Left != null)
+                {
+                    queue.Enqueue(temp.Left);
+                }
+                if (temp.Right != null)
+                {
+                    queue.Enqueue(temp.Right);
+                }
+            }
+        }
     }
 }
