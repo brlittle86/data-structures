476c4571dc32ffa4be2373d01846a1a860e70598
Brandon Little <brlittle86@gmail.com>
2017-10-17 16:55:48 -0700

    initial creation of files; added in traversals and Add from previous challenge

---
+++ BSTMinMaxDelete/BSTMinMaxDelete/BST.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSTMinMaxDelete
+{
+    class BST
+    {
+        public int Data { get; set; }
+        public BST Left { get; set; }
+        public BST Right { get; set; }
+
+        public BST(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+
+        public void BstAdd(int data)
+        {
+            if (data < Data)
+            {
+                if (Left == null)
+                {
+                    Left = new BST(data);
+                    return;
+                }
+                else
+                {
+                    Left.BstAdd(data);
+                    return;
+                }
+            }
+            else if (data > Data)
+            {
+                if (Right == null)
+                {
+                    Right = new BST(data);
+                    return;
+                }
+                else
+                {
+                    Right.BstAdd(data);
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Identical data found, no new leaf added.");
+            }
+        }
+
+        public void PreOrderTraversal()
+        {
+            Console.WriteLine(Data);
+            if (Left != null) { Left.PreOrderTraversal(); }
+            if (Right != null) { Right.PreOrderTraversal(); }
+        }
+
+        public void InOrderTraversal()
+        {
+            if (Left != null) { Left.PreOrderTraversal(); }
+            Console.WriteLine(Data);
+            if (Right != null) { Right.PreOrderTraversal(); }
+        }
+
+        public void PostOrderTraversal()
+        {
+            if (Left != null) { Left.PreOrderTraversal(); }
+            if (Right != null) { Right.PreOrderTraversal(); }
+            Console.WriteLine(Data);
+        }
+
+        public void BreadthTraversal()
+        {
+            Queue<BST> queue = new Queue<BST>();
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                BST temp = queue.Dequeue();
+                Console.Write(temp.Data + " ");
+
+                if (temp.Left != null)
+                {
+                    queue.Enqueue(temp.Left);
+                }
+                if (temp.Right != null)
+                {
+                    queue.Enqueue(temp.Right);
+                }
+            }
+        }
+    }
+}
---
+++ BSTMinMaxDelete/BSTMinMaxDelete/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BSTMinMaxDelete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST myTree = new BST(14);
+            myTree.BstAdd(9);
+            myTree.BstAdd(24);
+            myTree.BstAdd(7);
+            myTree.BstAdd(12);
+            myTree.BstAdd(17);
+            myTree.BstAdd(28);
+
+            /* Tree should look like:
+             *         14
+             *        /  \
+             *       9    24
+             *      / \   / \
+             *     7  12 17 28
+             */
+        }
+    }
+}
