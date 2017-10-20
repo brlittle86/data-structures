ebba1e8420c679e8bcde37e3f2246e65347d44eb
Brandon Little <brlittle86@gmail.com>
2017-10-20 14:54:01 -0700

    added basic files; created generically typed Node

---
+++ BSTAnimalGame/BSTAnimalGame/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSTAnimalGame
+{
+    class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
