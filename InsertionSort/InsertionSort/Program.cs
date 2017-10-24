3dccdf06029982711068ed4b1fe4913a2595cbe8
Brandon Little <brlittle86@gmail.com>
2017-10-23 17:26:58 -0700

    implemented InsertionSort method

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -8,5 +8,28 @@ namespace InsertionSort
         {
             Console.WriteLine("Hello World!");
         }
+
+        public int[] InsertionSort(int[] sortArray)
+        {
+            for (int i = 1; i < sortArray.Length; i++)
+            {
+                int j = i;
+                while (j > 0)
+                {
+                    if (sortArray[j - 1] > sortArray[j])
+                    {
+                        int temp = sortArray[j - 1];
+                        sortArray[j - 1] = sortArray[j];
+                        sortArray[j] = temp;
+                        j--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return sortArray;
+        }
     }
 }
