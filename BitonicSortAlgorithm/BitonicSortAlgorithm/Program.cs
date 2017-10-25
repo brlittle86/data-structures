0e97b7d8ed155cdfec0e58a9c98eb6d8ecb6ad1f
Brandon Little <brlittle86@gmail.com>
2017-10-25 09:48:34 -0700

    implemented Swap and Compare methods

--- BitonicSortAlgorithm/BitonicSortAlgorithm/Program.cs
+++ BitonicSortAlgorithm/BitonicSortAlgorithm/Program.cs
@@ -4,9 +4,27 @@ namespace BitonicSortAlgorithm
 {
     class Program
     {
+        const bool ascending = true;
+        const bool descending = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+
+        static void Compare(int[] array, int i, int j, bool dir)
+        {
+            if (dir == (array[i] > array[j]))
+            {
+                Swap(array, i, j);
+            }
         }
     }
 }
