05dbb9d28c8a255e6176deabfdb7636df204af08
Brandon Little <brlittle86@gmail.com>
2017-10-26 17:06:01 -0700

    initial file creation; implemented Partition and Swap methods

---
+++ QuickSortAlgorithm/QuickSortAlgorithm/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace QuickSortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            if (array[high] < array[i + 1])
+            {
+                Swap(array, i + 1, high);
+            }
+
+            return i + 1;
+        }
+
+        static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
+}
