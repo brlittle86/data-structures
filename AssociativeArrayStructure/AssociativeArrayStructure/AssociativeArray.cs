92e650fa75eecd3b23bfd5a8e5ab094b00410a4f
Brandon Little <brlittle86@gmail.com>
2017-10-31 17:08:01 -0700

    created AssociativeArray class

---
+++ AssociativeArrayStructure/AssociativeArrayStructure/AssociativeArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssociativeArrayStructure
+{
+    class AssociativeArray
+    {
+        public Object[] Kvp { get; set; }
+        public int CapacityFilled { get; set; }
+
+    }
+}
