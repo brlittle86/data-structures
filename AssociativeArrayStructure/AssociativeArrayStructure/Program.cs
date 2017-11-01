7975ce5f5e7509da0d40ad4c1b9cac799bf99c7a
Brandon Little <brlittle86@gmail.com>
2017-10-31 17:06:46 -0700

    initial solution creation

---
+++ AssociativeArrayStructure/AssociativeArrayStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssociativeArrayStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
