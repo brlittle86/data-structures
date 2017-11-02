8184df13bfbd880c82f1044c9ea8911c16ff903c
Brandon Little <brlittle86@gmail.com>
2017-11-02 16:51:49 -0700

    initial solution creation

---
+++ MatrixRotation/MatrixRotation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MatrixRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
