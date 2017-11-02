a46f7b063963c7e2b826772b7d793348c396b749
Brandon Little <brlittle86@gmail.com>
2017-11-01 17:18:00 -0700

    implemented AddToMatrix method; implemented a PopulateRandom method

--- MatrixArrayZero/MatrixArrayZero/MatrixArray.cs
+++ MatrixArrayZero/MatrixArrayZero/MatrixArray.cs
@@ -7,10 +7,47 @@ namespace MatrixArrayZero
     class MatrixArray
     {
         public int[,] MtxArr { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
 
-        public MatrixArray(int length, int width)
+        public MatrixArray(int width, int depth)
         {
-            MtxArr = new int[length, width];
+            MtxArr = new int[width, depth];
+            Width = width;
+            Depth = depth;
+        }
+
+        public void AddToMatrix(int x, int y, int value)
+        {
+            // If value is 0, set all values in row x and column y to 0, else set value at specified location
+            if (value == 0)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    MtxArr[i, y] = 0;
+                }
+                for (int j = 0; j < Depth; j++)
+                {
+                    MtxArr[x, j] = 0;
+                }
+            }
+            else
+            {
+                MtxArr[x, y] = value;
+            }
+        }
+
+        // Fills our matrix with random values between 1 and 10
+        public void PopulateRandom()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Depth; j++)
+                {
+                    MtxArr[i, j] = rnd.Next(1, 10);
+                }
+            }
         }
     }
 }
