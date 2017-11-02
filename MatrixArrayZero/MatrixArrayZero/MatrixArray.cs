1aed22c96b412875ea4c3bfd15ea37e43c6835e3
Brandon Little <brlittle86@gmail.com>
2017-11-01 17:02:46 -0700

    initial solution creation; created basic MatrixArray class

---
+++ MatrixArrayZero/MatrixArrayZero/MatrixArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixArrayZero
+{
+    class MatrixArray
+    {
+        public int[,] MtxArr { get; set; }
+
+        public MatrixArray(int length, int width)
+        {
+            MtxArr = new int[length, width];
+        }
+    }
+}
