b2f2422f259eda4fbed151339451cdb63a3ed360
Brandon Little <brlittle86@gmail.com>
2017-11-02 16:54:29 -0700

    implement previous Matrix methods

--- MatrixRotation/MatrixRotation/Matrix.cs
+++ MatrixRotation/MatrixRotation/Matrix.cs
@@ -16,5 +16,52 @@ namespace MatrixRotation
             Width = width;
             Depth = depth;
         }
+
+        public void AddToMatrix(int x, int y, int value)
+        {
+            // If value is 0, set all values in row x and column y to 0, else set value at specified location
+            if (value == 0)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    MtxArr[i, y] = 0;
+                }
+                for (int j = 0; j < Depth; j++)
+                {
+                    MtxArr[x, j] = 0;
+                }
+            }
+            else
+            {
+                MtxArr[x, y] = value;
+            }
+        }
+
+        // Fills our matrix with random values between 1 and 10
+        public void PopulateRandom()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Width; i++)
+            {
+                for (int j = 0; j < Depth; j++)
+                {
+                    MtxArr[i, j] = rnd.Next(1, 10);
+                }
+            }
+        }
+
+        // Print our matrix in an easily viewed format
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < Depth; i++)
+            {
+                string currentLine = "";
+                for (int j = 0; j < Width; j++)
+                {
+                    currentLine += $"{MtxArr[j, i]} ";
+                }
+                Console.WriteLine(currentLine);
+            }
+        }
     }
 }
