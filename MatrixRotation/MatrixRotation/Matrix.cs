c8f73a9d5c134e8730c816a8c9ac9ade19b690d4
Brandon Little <brlittle86@gmail.com>
2017-11-02 16:53:27 -0700

    basic Matrix implementation

---
+++ MatrixRotation/MatrixRotation/Matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixRotation
+{
+    class Matrix
+    {
+        public int[,] MtxArr { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
+
+        public Matrix(int width, int depth)
+        {
+            MtxArr = new int[width, depth];
+            Width = width;
+            Depth = depth;
+        }
+    }
+}
