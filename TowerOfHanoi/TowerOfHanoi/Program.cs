fe2ede7798f86ca1e595d48920f0fb522b899b6b
Brandon Little <brlittle86@gmail.com>
2017-11-07 10:22:04 -0800

    completed recursive function and added console interfacing to take in N disks for the tower and output the moves required

--- TowerOfHanoi/TowerOfHanoi/Program.cs
+++ TowerOfHanoi/TowerOfHanoi/Program.cs
@@ -6,7 +6,13 @@ namespace TowerOfHanoi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number of disks:");
+            int disks = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"For {disks} disks, make the following moves:");
+            Tower(disks, 1, 3);
+            int moves = Convert.ToInt32(Math.Pow(2, disks) - 1);
+            Console.WriteLine($"This took {moves} moves.");
+            Console.Read();
         }
 
         // Tower of Hanoi - takes in N values and moves them, one at a time, from one pole to a third pole, while ensuring that no value is stacked on top of a lower value.
@@ -16,7 +22,20 @@ namespace TowerOfHanoi
 
             if (n == 1)
             {
+                Console.WriteLine($"{fromStack} -> {toStack}");
+            }
+            else
+            {
+                helpStack = 6 - (fromStack + toStack);
 
+                // Move all but largest disk to the helpStack
+                Tower(n - 1, fromStack, helpStack);
+
+                // Move largest disk to destination stack
+                Console.WriteLine($"{fromStack} -> {toStack}");
+
+                // Move disks from helpStack to destination
+                Tower(n - 1, helpStack, toStack);
             }
         }
     }
