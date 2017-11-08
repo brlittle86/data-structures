8dbd9735411e5271df79b629bf67e1cf9e71bbec
Brandon Little <brlittle86@gmail.com>
2017-11-07 16:20:22 -0800

    added Animal class with properties

---
+++ AnimalAdoption/AnimalAdoption/Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalAdoption
+{
+    class Animal
+    {
+        public string Species { get; set; }
+        public int IntakeNumber { get; set; }
+    }
+}
