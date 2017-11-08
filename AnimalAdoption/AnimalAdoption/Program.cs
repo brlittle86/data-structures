c416e7526ce2137d54c91cb81f36da37d580fa80
Brandon Little <brlittle86@gmail.com>
2017-11-07 16:14:36 -0800

    initial solution creation

---
+++ AnimalAdoption/AnimalAdoption/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimalAdoption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
