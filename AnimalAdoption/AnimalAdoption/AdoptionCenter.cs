233819f5ab72ec18f1ed6d94562ed488cb34d483
Brandon Little <brlittle86@gmail.com>
2017-11-07 16:17:44 -0800

    created AdoptionCenter class with properties and constructor

---
+++ AnimalAdoption/AnimalAdoption/AdoptionCenter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalAdoption
+{
+    class AdoptionCenter
+    {
+        public Queue<Animal> Cats { get; set; }
+        public Queue<Animal> Dogs { get; set; }
+        public int LinePosition { get; set; }
+
+        public AdoptionCenter()
+        {
+            Cats = new Queue<Animal>();
+            Dogs = new Queue<Animal>();
+            LinePosition = 0;
+        }
+    }
+}
